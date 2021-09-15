972836977c4ee7fc18f744218faa169b3c96c822
carterdealma <85889648+carterdealma@users.noreply.github.com>
2021-09-15 13:40:13 -0600

    Created SystemsAndImpact Repository

---
+++ SystemsAndImpact/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  SystemsAndImpact
+//
+//  Created by Ma, Carter on 9/15/21.
+//
+
+import SwiftUI
+
+struct ContentView: View
+{
+    var body: some View
+    {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider
+{
+    static var previews: some View
+    {
+        ContentView()
+    }
+}
---
+++ SystemsAndImpact/Resources/SystemsAndImpactApp.swift
@@ -0,0 +1,17 @@
+//
+//  SystemsAndImpactApp.swift
+//  SystemsAndImpact
+//
+//  Created by Ma, Carter on 9/15/21.
+//
+
+import SwiftUI
+
+@main
+struct SystemsAndImpactApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
