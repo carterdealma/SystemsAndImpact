c32a1d3448054215b9844e090eaa519fc81be342
carterdealma <85889648+carterdealma@users.noreply.github.com>
2021-09-17 13:53:52 -0600

    Renamed ImpactAndSystemsView

--- SystemsAndImpact/ImpactAndSystemsView.swift
+++ SystemsAndImpact/ImpactAndSystemsView.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View
+struct ImpactAndSystemsView: View
 {
     var body: some View
     {
@@ -16,10 +16,10 @@ struct ContentView: View
     }
 }
 
-struct ContentView_Previews: PreviewProvider
+struct ImpactAndSystemsView_Previews: PreviewProvider
 {
     static var previews: some View
     {
-        ContentView()
+        ImpactAndSystemsView()
     }
 }
