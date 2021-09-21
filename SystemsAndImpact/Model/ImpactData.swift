50e2f5c0ab4453aa2319acb9bc8ec99c317200a1
carterdealma <85889648+carterdealma@users.noreply.github.com>
2021-09-21 12:10:05 -0600

    Added Coordinates, Titles, and Details in ImpactData.swift

--- SystemsAndImpact/Model/ImpactData.swift
+++ SystemsAndImpact/Model/ImpactData.swift
@@ -5,4 +5,38 @@
 //  Created by Ma, Carter on 9/21/21.
 //
 
-import Foundation
+import MapKit
+
+func loadImpactData() -> [ComputingImpact]
+{
+    var impactItems : [ComputingImpact] = []
+    
+    let regions : [CLLocationCoordinate2D] =
+    [
+        CLLocationCoordinate2D(latitude: 20.5937, longitude: 78.9629),
+        CLLocationCoordinate2D(latitude: 61.5240, longitude: 105.3188),
+        CLLocationCoordinate2D(latitude: 34.0479, longitude: 100.6197),
+        CLLocationCoordinate2D(latitude: 37.0902, longitude: 95.7129),
+        CLLocationCoordinate2D(latitude: 37.3875, longitude: 122.0575)
+    ]
+    
+    let titles : [String] =
+    [
+        "Gender Inequity in Social Media Voice Forums",
+        "Russian Interference in the U.S Election",
+        "Automation of Industrial Jobs in Asia",
+        "America's Moon Landing",
+        "Environmental Risk in Silicon Valley",
+    ]
+    
+    let details : [String] =
+    [
+        "Women face systematic discrimination and encountered abuse content, flirts, threats and harrassment.",
+        "Alleged that the Russian government illicitly collected emails and documents of the DNC.",
+        "There is expected to be a 16% growth in industrial robots jobs in Asia between 2019 and 2021.",
+        "The Apollo Guidance Computer (ACG) played a very significant role in America's successful moon landing.",
+        "Silicon Valley moved from an agricultural industry to a high-tech industry very fast. This lead to heavy metals and toxic chemicals to pollute the soil and contaminate groundwater.",
+    ]
+    
+    return impactItems
+}
